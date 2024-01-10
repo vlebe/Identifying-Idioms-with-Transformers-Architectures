8b948aa89043e8fe7260efeca53382ed8fb54f98
Méliya <148862817+ayilem@users.noreply.github.com>
2024-01-10 08:12:25 +0100

    Update dataset2.py

--- dataset2.py
+++ dataset2.py
@@ -33,3 +33,37 @@ class MWEDataset(Dataset):
         print(f"input_ids length: {input_ids.size(1)}, attention_mask length: {attention_mask.size(1)}")
         return input_ids, attention_mask, labels
         
+
+
+
+def collate_batch(batch):
+    input_ids, attention_masks, labels = zip(*batch)
+
+    # d'abord on convertit les listes en tenseurs et les pad si nécessaire
+    input_ids = [ids.clone().detach() for ids in input_ids]
+    attention_masks = [mask.clone().detach() for mask in attention_masks]
+
+
+    # icii on gère le cas où il y a une seule entrée dans une séquence
+    input_ids = [ids if ids.ndim > 1 else ids.unsqueeze(0) for ids in input_ids]
+    attention_masks = [mask if mask.ndim > 1 else mask.unsqueeze(0) for mask in attention_masks]
+
+    # on pad les séquences pour qu'elles aient toutes la même longueur
+    input_ids = pad_sequence(input_ids, batch_first=True, padding_value=0)
+    attention_masks = pad_sequence(attention_masks, batch_first=True, padding_value=0)
+    labels = pad_sequence(labels, batch_first=True, padding_value=-100)
+
+    return input_ids, attention_masks, labels
+
+
+
+if __name__ == "__main__":
+    tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+    dataset = MWEDataset("train_IGO.csv", tokenizer)
+    data_loader = DataLoader(dataset, batch_size=32, collate_fn=collate_batch)
+
+    for input_ids, attention_mask, labels in data_loader:
+        print(input_ids.shape, attention_mask.shape, labels.shape)
+
+
+
